29af23f86b556f841274d73561a4c5378b5e2ff9
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-11-03 19:46:27 -0500

    Programa 4

--- api.php
+++ api.php
@@ -1,66 +1,18 @@
 <?php
-    header('Content-Type: application/json; charset=utf-8');
+  
+  header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
-    class promedio{
 
-        public $sumA;
-        public $canA;
-        public $sumB;
-        public $canB;
-        public $sumC;
-        public $canC;
-
-        public function __construct( nt $sumA,int $canA,int $sumB,int $canB, int $sumC,int $canC){
-            $this->sumA = $sumA;
-            $this->canA = $canA;
-            $this->sumB = $sumB;
-            $this->canB = $canB;
-            $this->sumC = $sumC;
-            $this->canC = $canC;
-        }
-
-
-        public function promedioF():string{
-
-            $sumA = $this->sumA;
-            $canA = $this->canA;
-            $sumB = $this->sumB;
-            $canB = $this->canB;
-            $sumC = $this->sumC;
-            $canC = $this->canC;
-
-            $canA<1?$proA=0:$proA = $sumA/$canA;
-            $canB<1?$proB=0:$proB = $sumB/$canB;
-            $canC<1?$proC=0:$proC = $sumC/$canC;
-
-            if($proA>$proB && $proA>$proC){
-                if($proB>$proC){
-                    return "C";
-                } else{
-                    return "B";
-                }
-            } else if($proB>$proA && $proB>$proC){
-                if($proA>$proC){
-                    return "C";
-                } else{
-                    return "A";
-                }
-            } else if($proC>$proA && $proC>$proB){
-                if($proA>$proB){
-                    return "B";
-                } else{
-                    return "A";
-                }
-            } else{
-                return "A, B, C";
-            }
+    class pago{
+        public function __construct(){
         }
     }
-    $lista = new promedio(sumA:$_DATA[0],canA:$_DATA[1],sumB:$_DATA[2],canB:$_DATA[3],sumC:$_DATA[4],canC:$_DATA[5]);
-    echo($lista->promedioF());
-?>
+    $lista = new pago();
+
 
 ?>
+
+
 
 
 
--- main.js
+++ main.js
@@ -1,87 +1,25 @@
-let sumA=0
-let sumB=0
-let sumC=0
-let canA=0
-let canB=0
-let canC=0;
-
-function sumarPromedio(sexo, carrera, edad) {
-    if(sexo=="M"){
-        if(carrera=="a"){
-            sumA= sumA+edad;
-            canA +=1;
-        }else if (carrera=="b"){
-            sumB= sumB+edad;
-            canB +=1;
-        }else{
-            sumC= sumC+edad;
-            canC +=1;
-        }
-    } else{
-        null
-    }
-}
-addEventListener("DOMContentLoaded", (e)=>{
-    document.querySelector("#alumnos").removeAttribute("disabled");
-   
-    document.querySelector("#alumnos").addEventListener("keyup", (e) => {
-        let sexo = document.querySelector("#sexo");
-        let edad = document.querySelector("#edad");
-        let carrera = document.querySelector("#carrera");
-        if (e.target.value > 0) {
-            sexo.removeAttribute("disabled");
-            edad.removeAttribute("disabled");
-            carrera.removeAttribute("disabled");
-        } else {
-            sex0.disabled = true;
-            edad.disabled = true;
-            carrera.disabled = true;
-        }
-    })
-    let con = 1;
-    let lista = [];
-    let myform = document.querySelector("#formulario");
-    myform.addEventListener("submit", async (e) => {
+addEventListener("DOMContentLoaded", (e) => {
+    let form = document.querySelector("#form");
+    form.addEventListener("submit", async (e) => {
         e.preventDefault();
-        let alumnos = document.querySelector("#alunmos").value;
-        document.querySelector("#alumnos").disabled = true;
-        if (con < alumnos) {
-            let data = Object.fromEntries(new FormData(formulario));
-            sumaPro(data["sexo"], data["carrera"], parseInt(data["edad"]));
-            document.querySelector("#sexo").selectedIndex = 0;
-            document.querySelector("#edad").value = "";
-            document.querySelector("#carrera").selectedIndex = 0;
-            con += 1;
-
-        } else {
-            let form = e.target;
-            let data = Object.fromEntries(new FormData(formulario));
-            sumaPro(data["sexo"], data["carrera"], parseInt(data["edad"]));
-            document.querySelector("#resultado").innerHTML = "";
-            lista.push(sumaA);
-            lista.push(canA);
-            lista.push(sumaB);
-            lista.push(canB);
-            lista.push(sumaC);
-            lista.push(canC);
-
-            let config = {
-                method: form.method,
-                body: JSON.stringify(lista)
-            };
-
-            let peticion = await fetch(formulario.action, config);
-            let respuesta = await peticion.text();
-            document.querySelector("#resultado").insertAdjacentHTML("beforeend", respuesta);
-            document.querySelector("#sexo").selectedIndex = 0;
-            document.querySelector("#edad").value = "";
-            document.querySelector("#carrera").selectedIndex = 0;
-            sexo.disabled = true;
-            edad.disabled = true;
-            carrera.disabled = true;
+        let meses = document.querySelector("#meses").value;
+        let dato = new Date(meses);
+        let year,mes,dia;
+        var pago = 10;
+        if (!!dato.valueOf()) {
+            year = dato.getFullYear();
+            mes = dato.getMonth() + 1;
+            dia = dato.getDate() + 1;
         }
-
+        let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+        let nombreMes = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+        for(let i=0;i<20;i++){
+            var indice = new Date(year, mes - 1, 16).getDay();
+            document.querySelector("#res").insertAdjacentHTML("beforeend",(`${diasSemana[indice]} del ${dia} de ${nombreMes[new Date(year, mes - 1, dia).getMonth()]} del ${(new Date(year, mes - 1, dia).getMonth()==0) ?++year : year} debe pagar ${pago}<br>`));
+            pago=pago*2;
+            mes++;
+        }
     })
-})    
+})
 
 
