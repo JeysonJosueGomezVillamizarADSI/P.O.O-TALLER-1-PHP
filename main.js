8598d25f7e96dc4d2a3d6fe4b353cdd92f7f23b3
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-11-06 13:18:06 -0500

    Programa 6

--- main.js
+++ main.js
@@ -1,43 +1,47 @@
-let ventaMen=0,ventaMed=0,ventaSup=0,canMen=0,canMed=0,canSup=0,ventaTotal=0;
-
-function sumaVentas(precio){
-    precio<=500?
-        (ventaMen+=precio,canMen++):
-            precio<=1000 && precio>500?
-                (ventaMed+=precio,canMed++):
-                (ventaSup+=precio,canSup++);
-    ventaTotal=ventaMen+ventaMed+ventaSup;
+function masa(presion,volumen){
+    return presion*volumen;
 }
 
 addEventListener("DOMContentLoaded", (e) => {
     document.querySelector("#cantidad").removeAttribute("disabled");
     document.querySelector("#cantidad").addEventListener("keyup", (e) => {
-        let precio = document.querySelector("#precio");
+        let presion = document.querySelector("#presion");
+        let volumen = document.querySelector("#volumen");
         if (e.target.value > 0) {
-            precio.removeAttribute("disabled");
+            presion.removeAttribute("disabled");
+            volumen.removeAttribute("disabled");
         } else {
-            precio.disabled = true;
+            presion.disabled = true;
+            volumen.disabled = true;
         }
     })
-    let con = 1;
+    let con = 1,lista=[];
     let myform = document.querySelector("#form");
     myform.addEventListener("submit", async (e) => {
         e.preventDefault();
         let cantidad = document.querySelector("#cantidad").value;
-        let precio = parseInt( document.querySelector("#precio").value);
+        let presion = parseInt( document.querySelector("#presion").value);
+        let volumen = parseInt( document.querySelector("#volumen").value);
         document.querySelector("#cantidad").disabled = true;
         if (con < cantidad) {
-            document.querySelector("#precio").value = "";
+            document.querySelector("#presion").value = "";
+            document.querySelector("#volumen").value = "";
             con += 1;
-            sumaVentas(precio);
+            lista.push(masa(presion,volumen));
         } else {
-            document.querySelector("#res").innerHTML = "";
-            sumaVentas(precio);
-            document.querySelector("#res").insertAdjacentHTML
-                ("beforeend",`La cantidad de ventas superiores a 1000 son ${canSup} y su monto respectivo es ${ventaSup} <br>`+
-                `La cantidad de ventas superiores a 500 y menores a 1000 son ${canMed} y su monto respectivo es ${ventaMed} <br>`+
-                `La cantidad de ventas menores a 500 son ${canMen} y su monto respectivo es ${ventaMen} <br>`+
-                `La venta total es de ${ventaTotal}`);
+            let form = e.target;
+            lista.push(masa(presion,volumen));
+            document.querySelector("#presion").disabled=true;
+            document.querySelector("#volumen").disabled=true;
+            let config = {
+                method: form.method,
+                body: JSON.stringify(lista)
+            };
+
+            let peticion = await fetch(form.action, config);
+            let respuesta = await peticion.text();
+            document.querySelector("#res").insertAdjacentHTML("beforeend",respuesta)
         }
     })
 })
+
