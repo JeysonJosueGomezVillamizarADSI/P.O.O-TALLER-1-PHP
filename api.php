a1b4d2c340949b565a6ab84e26300ca3ac7b7cf5
JeysonJosueGomezVillamizarADSI <aminoterlo@gmail.com>
2022-11-05 20:48:15 -0500

    Programa 5

--- api.php
+++ api.php
@@ -1,15 +1,17 @@
 <?php
-  
-  header('Content-Type: application/json; charset=utf-8');
+    header("Access-Control-Allow-Origin: *");
+    header('Content-Type: application/json; charset=utf-8');
     $_DATA = json_decode(file_get_contents("php://input"),true);
 
     class pago{
-        public function __construct(){
+
+        public function __construct($lista){
+
         }
+
+        public function ventas():string{
     }
-    $lista = new pago();
-
-
+}
 ?>
 
 
--- main.js
+++ main.js
@@ -1,25 +1,43 @@
+let ventaMen=0,ventaMed=0,ventaSup=0,canMen=0,canMed=0,canSup=0,ventaTotal=0;
+
+function sumaVentas(precio){
+    precio<=500?
+        (ventaMen+=precio,canMen++):
+            precio<=1000 && precio>500?
+                (ventaMed+=precio,canMed++):
+                (ventaSup+=precio,canSup++);
+    ventaTotal=ventaMen+ventaMed+ventaSup;
+}
+
 addEventListener("DOMContentLoaded", (e) => {
-    let form = document.querySelector("#form");
-    form.addEventListener("submit", async (e) => {
-        e.preventDefault();
-        let meses = document.querySelector("#meses").value;
-        let dato = new Date(meses);
-        let year,mes,dia;
-        var pago = 10;
-        if (!!dato.valueOf()) {
-            year = dato.getFullYear();
-            mes = dato.getMonth() + 1;
-            dia = dato.getDate() + 1;
+    document.querySelector("#cantidad").removeAttribute("disabled");
+    document.querySelector("#cantidad").addEventListener("keyup", (e) => {
+        let precio = document.querySelector("#precio");
+        if (e.target.value > 0) {
+            precio.removeAttribute("disabled");
+        } else {
+            precio.disabled = true;
         }
-        let diasSemana = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-        let nombreMes = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
-        for(let i=0;i<20;i++){
-            var indice = new Date(year, mes - 1, 16).getDay();
-            document.querySelector("#res").insertAdjacentHTML("beforeend",(`${diasSemana[indice]} del ${dia} de ${nombreMes[new Date(year, mes - 1, dia).getMonth()]} del ${(new Date(year, mes - 1, dia).getMonth()==0) ?++year : year} debe pagar ${pago}<br>`));
-            pago=pago*2;
-            mes++;
+    })
+    let con = 1;
+    let myform = document.querySelector("#form");
+    myform.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        let cantidad = document.querySelector("#cantidad").value;
+        let precio = parseInt( document.querySelector("#precio").value);
+        document.querySelector("#cantidad").disabled = true;
+        if (con < cantidad) {
+            document.querySelector("#precio").value = "";
+            con += 1;
+            sumaVentas(precio);
+        } else {
+            document.querySelector("#res").innerHTML = "";
+            sumaVentas(precio);
+            document.querySelector("#res").insertAdjacentHTML
+                ("beforeend",`La cantidad de ventas superiores a 1000 son ${canSup} y su monto respectivo es ${ventaSup} <br>`+
+                `La cantidad de ventas superiores a 500 y menores a 1000 son ${canMed} y su monto respectivo es ${ventaMed} <br>`+
+                `La cantidad de ventas menores a 500 son ${canMen} y su monto respectivo es ${ventaMen} <br>`+
+                `La venta total es de ${ventaTotal}`);
         }
     })
 })
-
-
